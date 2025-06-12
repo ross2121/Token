1ae0c5b45e1cddafd68cd5722f839dac40d99ecb
Youval Singh <youvalsingh40@gmail.com>
2025-06-12 23:03:40 +0530

    init token with test

--- programs/token/src/lib.rs
+++ programs/token/src/lib.rs
@@ -6,10 +6,17 @@ use anchor_spl::{
 };
 
 declare_id!("Gek9iUND53ww6PB5jxWa5XB5ndnS9QZuVzrszwPGJtWp");
+#[constant]
+pub const NAME: &str="TOKEN LOTTERY TICKET";
+
+#[constant]
+pub const SYMBOL:&str="TLT";
+#[constant]
+pub const  URI:&str="https://www.edepotindia.com/wp-content/uploads/2018/12/west-bengal-lottery-ticket.jpg";
 
 #[program]
 pub mod token {
-    use anchor_spl::token_2022::{mint_to, MintTo};
+    use anchor_spl::{metadata::{create_master_edition_v3, create_metadata_accounts_v3, mpl_token_metadata::types::{CollectionDetails, Creator, DataV2}, sign_metadata, CreateMasterEditionV3, CreateMetadataAccountsV3, SignMetadata}, token_2022::{mint_to, MintTo}};
 
     use super::*;
 
@@ -28,10 +35,47 @@ pub mod token {
     
     pub fn initialize_lottery(ctx: Context<InitializeToken>) -> Result<()> {
           let signer_seeds:&[&[&[u8]]]=&[&[b"collectionmint",&[ctx.bumps.collection_mint],]];
+        
           mint_to(
             CpiContext::new_with_signer(ctx.accounts.token_program.to_account_info(),
              MintTo{
                 mint:ctx.accounts.collection_mint.to_account_info(),to:ctx.accounts.collection_token.to_account_info(),authority:ctx.accounts.collection_mint.to_account_info()}, signer_seeds),1)?;
+                      
+                create_metadata_accounts_v3(CpiContext::new_with_signer(
+                    ctx.accounts.token_metadata_program.to_account_info(),CreateMetadataAccountsV3{
+                    metadata:ctx.accounts.metadata.to_account_info(),
+                    mint:ctx.accounts.collection_mint.to_account_info(),
+                    mint_authority:ctx.accounts.collection_mint.to_account_info(),
+                     payer:ctx.accounts.signer.to_account_info(),
+                     update_authority:ctx.accounts.collection_mint.to_account_info(),
+                     system_program:ctx.accounts.system_program.to_account_info(),
+                     rent:ctx.accounts.rent.to_account_info()
+                }, signer_seeds),DataV2{
+                         name:NAME.to_string(),
+                         symbol:SYMBOL.to_string(),
+                         uri:URI.to_string(),
+                         seller_fee_basis_points:0,
+                         creators:Some(vec![Creator {
+                            address:ctx.accounts.collection_mint.key(),
+                            verified:false,
+                            share:100
+                         }]),
+                         collection:None,
+                         uses:None 
+                },true, true, Some(CollectionDetails::V1 {size:0}))?;
+                
+                create_master_edition_v3(CpiContext::new_with_signer(ctx.accounts.token_metadata_program.to_account_info(),CreateMasterEditionV3{
+                    edition:ctx.accounts.master_edition.to_account_info(),
+                    mint:ctx.accounts.collection_mint.to_account_info(),
+                    update_authority:ctx.accounts.collection_mint.to_account_info(),
+                    payer:ctx.accounts.signer.to_account_info(),
+                    token_program:ctx.accounts.token_program.to_account_info(),
+                    system_program:ctx.accounts.system_program.to_account_info(),
+                    rent:ctx.accounts.rent.to_account_info(),
+                    metadata:ctx.accounts.metadata.to_account_info(),
+                    mint_authority:ctx.accounts.collection_mint.to_account_info()
+                }, signer_seeds),Some(0))?;
+                sign_metadata(CpiContext::new_with_signer(ctx.accounts.token_metadata_program.to_account_info(),SignMetadata{creator:ctx.accounts.collection_mint.to_account_info(),metadata:ctx.accounts.metadata.to_account_info()}, signer_seeds))?;
         Ok(())
     }
 }
@@ -96,7 +140,6 @@ pub struct InitializeToken<'info> {
     )]
     /// CHECK: This account is checked by the metadata program
     pub metadata: UncheckedAccount<'info>,
-    
     #[account(
         mut,
         seeds = [
@@ -114,5 +157,6 @@ pub struct InitializeToken<'info> {
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
     pub token_metadata_program: Program<'info, Metadata>,
-    pub system_program: Program<'info, System>
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
 }
---
+++ tests/token.ts
@@ -0,0 +1,45 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Token } from "../target/types/token";
+
+describe("token", () => {
+  // Configure the client to use the local cluster.
+
+  const provider=anchor.AnchorProvider.env();
+  anchor.setProvider(anchor.AnchorProvider.env());
+ const wallet=provider.wallet as anchor.Wallet;
+
+  const program = anchor.workspace.token as Program<Token>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx =await program.methods.initialize(
+      new anchor.BN(0),
+      new anchor.BN(12313),
+      new anchor.BN(232)
+    ).instruction();
+    const blockhash=await provider.connection.getLatestBlockhash();
+    const tx3=new anchor.web3.Transaction(
+      {
+        blockhash:blockhash.blockhash,
+        feePayer:provider.wallet.publicKey,
+        lastValidBlockHeight:blockhash.lastValidBlockHeight  
+      }  
+    ).add(tx);
+    const sign=await anchor.web3.sendAndConfirmTransaction(provider.connection,tx3,[wallet.payer]);
+    const initlottery=await program.methods.initializeLottery().accounts({
+      
+    }).instruction();
+    const initlotterty=new anchor.web3.Transaction(
+      {
+        blockhash:blockhash.blockhash,
+        feePayer:provider.wallet.publicKey,
+        lastValidBlockHeight:blockhash.lastValidBlockHeight  
+      }  
+    ).add(initlottery);
+    const initsign=await anchor.web3.sendAndConfirmTransaction(provider.connection,initlotterty,[wallet.payer]);
+    console.log("dasd",wallet.payer.publicKey.toBase58())
+    console.log("dasd",initsign);
+    console.log("Your transaction signature", sign);
+  });
+});  
