2c562203dc359d1254f7913a3a75b1908383f172
Youval Singh <youvalsingh40@gmail.com>
2025-06-14 12:16:07 +0530

    bug of buy ticket fix

--- programs/token/src/lib.rs
+++ programs/token/src/lib.rs
@@ -28,7 +28,6 @@ pub mod token {
       ctx.accounts.token_lottery.ticket_price=price;
       ctx.accounts.token_lottery.winner_claimed=false;
       ctx.accounts.token_lottery.randomness_account=Pubkey::default();
-       
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
@@ -75,6 +74,7 @@ pub mod token {
                     metadata:ctx.accounts.metadata.to_account_info(),
                     mint_authority:ctx.accounts.collection_mint.to_account_info()
                 }, signer_seeds),Some(0))?;
+                
                 sign_metadata(CpiContext::new_with_signer(ctx.accounts.token_metadata_program.to_account_info(),SignMetadata{creator:ctx.accounts.collection_mint.to_account_info(),metadata:ctx.accounts.metadata.to_account_info()}, signer_seeds))?;
         Ok(())
     }
@@ -89,6 +89,54 @@ bump)]
 pub token_lottery:Account<'info,TokenLottery>,
 pub system_program:Program<'info,System>
 
+}
+#[derive(Accounts)]
+pub struct Buyticket<'info>{
+    #[account(mut)]
+    pub payer:Signer<'info>,
+    #[account(mut,
+    seeds=[b"token_lottery".as_ref()],
+    bump=token_lottery.bump
+    )]
+    pub token_lottery:Account<'info,TokenLottery>,
+    #[account(init,payer=payer,seeds=[token_lottery.total_tickets.to_le_bytes().as_ref()],bump,
+    mint::decimals=0,mint::authority=collection_mint,mint::freeze_authority=collection_mint,mint::token_program=token_program
+   )]
+   pub ticket_mint:Account<'info,Mint>,
+   #[account(
+    mut,
+    seeds = [
+        b"metadata", 
+        token_metadata_program.key().as_ref(),
+        ticket_mint.key().as_ref()
+    ],
+    bump,
+    seeds::program = token_metadata_program.key()
+)]
+/// CHECK: This account is checked by the metadata program
+pub ticket_metadata: UncheckedAccount<'info>,
+#[account(
+    mut,
+    seeds = [
+        b"metadata", 
+        token_metadata_program.key().as_ref(),
+        ticket_mint.key().as_ref(),
+        b"edition"
+    ],
+    bump,
+    seeds::program = token_metadata_program.key()
+)]
+/// CHECK: This account is checked by the metadata program
+pub ticket_master_edition: UncheckedAccount<'info>,
+#[account(init,payer=payer,associated_token::mint=ticket_mint,associated_token::authority=payer,associated_token::token_program=token_program)]
+pub destination:Account<'info,TokenAccount>,
+   #[account(mut,seeds=[b"collectionmint".as_ref()],bump)]
+   pub collection_mint:Account<'info,Mint>,
+       pub system_program:Program<'info,System>,
+       pub associated_token_program:Program<'info,AssociatedToken>,
+       pub token_metadata_program: Program<'info, Metadata>,
+    pub token_program:Program<'info,Token>,
+    pub rent: Sysvar<'info, Rent>,
 }
 #[account]
 #[derive(InitSpace)]
@@ -105,10 +153,10 @@ pub struct  TokenLottery{
      
 }
 #[derive(Accounts)]
+
 pub struct InitializeToken<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
-    
     #[account(
         init,
         payer = signer,
@@ -119,7 +167,6 @@ pub struct InitializeToken<'info> {
         bump
     )]
     pub collection_mint: Account<'info, Mint>,
-    
     #[account(
         init,
         payer = signer,
@@ -127,7 +174,6 @@ pub struct InitializeToken<'info> {
         associated_token::authority = signer
     )]
     pub collection_token: Account<'info, TokenAccount>,
-    
     #[account(
         mut,
         seeds = [
@@ -153,7 +199,6 @@ pub struct InitializeToken<'info> {
     )]
     /// CHECK: This account is checked by the metadata program
     pub master_edition: UncheckedAccount<'info>,
-    
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
     pub token_metadata_program: Program<'info, Metadata>,
